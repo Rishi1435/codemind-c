49ecc2db744b61cfd6d6b13f1cb82ceb5d789844
Rishi1435 <147487529+Rishi1435@users.noreply.github.com>
2023-12-04 11:54:29 +0530

    #include<stdio.h> int main() {
        while(1)
        {
        int n;
        scanf("%d",&n);
            
            if(n==-1)
            {
            break;
            }
            printf("%d
    ",n*n);
        }
    }

---
+++ Infinite.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    while(1)
+    {
+    int n;
+    scanf("%d",&n);
+        
+        if(n==-1)
+        {
+        break;
+        }
+        printf("%d
+",n*n);
+    }
+}
